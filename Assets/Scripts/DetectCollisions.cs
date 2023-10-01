52c40f170d5d4c39e0f6a23b2c6cb6363a087030
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-01 12:50:10 +0100

    spawn with sliders...
    but they don't work yet

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -7,31 +7,35 @@ public class DetectCollisions : MonoBehaviour
     private GameManager gameManager;
 
     void Awake()
-    {//intermittently losing Game Manager script during gameplay, trying to resolve by moving call to Game Manager to awake instead of start
-        if (gameManager == null)
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); 
-        {
-            Debug.LogError("GameManager not found!"); //trying to solve a null object error in my scene
-        }
-    }
-     void Start()
-     {
-       
-     }
-
-    private void OnTriggerEnter(Collider other)
+{
+    // intermittently losing Game Manager script during gameplay, trying to resolve by moving call to Game Manager to awake instead of start
+    if (gameManager == null)
     {
-        if (other.CompareTag("Player"))
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); 
+        if (gameManager == null)
         {
-            gameManager.AddLives(-1);
-            Destroy(gameObject);
+            Debug.LogError("GameManager not found!"); // trying to solve a null object error in my scene
         }
-        else if (other.CompareTag("Animal"))
-        {
-            gameManager.AddScore(5);
-            Destroy(gameObject);
-            Destroy(other.gameObject);
-        }
-       
     }
+}
+
+void Start()
+{
+
+}
+
+private void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Player"))
+    {
+        gameManager.AddLives(-1);
+        Destroy(gameObject);
+    }
+    else if (other.CompareTag("Animal"))
+    {
+       other.GetComponent<AnimalHunger>().FeedAnimal(1);
+        Destroy(gameObject);
+      
+    }
+}
 }
